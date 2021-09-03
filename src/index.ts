6d03331c06d4318b0ef69036a4a7bfa4a773407d
ampod1 <59267320+ampod1@users.noreply.github.com>
2021-09-03 16:54:44 +0200

    casc

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import inflection from "inflection";
 // Best way to do it is not found yet , so do it with the most practical way
 const createElementFromColumn = ({ column_name }: ColumnType) => {
   const ComponentName = inflection.camelize(column_name);
-  return `<${ComponentName}><br/></${ComponentName}>`;
+  return `<${ComponentName}><hr/></${ComponentName}>`;
 };
 
 const createListFromResource = () => {
